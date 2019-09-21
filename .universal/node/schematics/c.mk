45107d41a81d78cc241f57f5e846a7f0906d6ee2
christopher donlan <chris.donlan87@gmail.com>
2019-09-21 09:44:07 -0400

    added the c schematic

---
+++ .universal/node/schematics/c.mk
@@ -0,0 +1,20 @@
+.PHONY:
+construct-node-c: src lib exe obj .link .include
+
+src:
+	mkdir src
+
+lib:
+	mkdir lib
+
+exe:
+	mkdir exe
+
+obj:
+	mkdir obj
+
+.link:
+	touch .link
+
+.include:
+	touch .include
