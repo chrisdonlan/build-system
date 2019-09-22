9c5367df3c25f601638cde9795ed3efad0d458b0
christopher donlan <chris.donlan87@gmail.com>
2019-09-22 11:45:24 -0400

    updates to buildsys

--- .universal/node/makes/c.mk
+++ .universal/node/makes/c.mk
@@ -1,11 +1,14 @@
 ifndef __CMAKE_MK__
 
-mainfiles=$(shell find src -name *.c -type f -printf "%f ")
-srcnames=$(shell find src -name *.c -type f -printf "%f ")
+mainfiles=$(shell find main -name *.c -type f -printf "%p ")
+mainames=$(shell find main -name *.c -type f -printf "%f\n" | xargs -I arg basename -s .c arg)
+srcfiles=$(shell find src -name *.c -type f -printf "%p ")
+srcnames=$(shell find src -name *.c -type f -printf "%f\n" | xargs -I arg basename -s .c arg)
 objects=$(foreach name,$(srcnames),obj/$(name).o)
 executables=$(foreach name,$(mainames),exe/$(name))
 # NAME is defined in .universal/cfg.mk and references the node
-lib=lib/${NAME}.a
+lib=$(shell if [ ! -z "$(objects)" ]; then echo lib/${NAME}.a; fi)
+links=$(foreach l,${lib},-Llib -l$(shell basename -s .a ${l}))
 
 
 obj/%.o: src/$$*.c
@@ -15,10 +18,12 @@ obj/%.o: src/$$*.c
 lib/lib%.a: $(objects)
 	-ar -rcs $@ $(objects)
 
-exe/%:
-	gcc -Isrc -Llib -l${PROJ} -o $@ main/$*.c 
-	
+exe/%: ${mainfiles}
+	gcc -Isrc ${links} -o $@ main/$*.c
+
+.PHONY:
 c-build: ${lib} | $(executables)
+
 
 __CMAKE_MK__:=1
 endif
